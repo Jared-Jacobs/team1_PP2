70a467f03b2a9d2af2535ed7558b0308883ce7ca
ArtfulDodge <jcoffey97@gmail.com>
2019-10-28 09:31:00 -0400

    minor fix to grammar to allow for comments

---
+++ kcExample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class kcExample
+{
+public static void main(String[] args)
+{
+Scanner scan = new Scanner(System.in);
+int
+x;
+int
+y;
+int
+z;
+x=10
+;
+y=12
+;
+z=x+y
+;
+System.out.println(z)
+;
+}
+}
